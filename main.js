5dfea580ce6c9fffdbb22d37da98011e4a2169b5
Lisa-2011 <93234101+Lisa-2011@users.noreply.github.com>
2023-03-04 10:20:48 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,69 +2,68 @@ function startClassification()
 { 
     navigator.mediaDevices.getUserMedia({ audio: true});
      classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/kA4F-80yW/model.json', modelReady); } 
-function modelReady(){
-     classifier.classify( gotResults); }
-
+     modelReady();
+     
+     function modelReady(){
+     classifier.classify( gotResults);}
      function gotResults(error,results){
         if(error){
             console.error(error);
         }
         else{
             console.log(results);
-        }
+            var red= Math.floor(Math.random()* 255 +1);
+            var blue=Math.floor(Math.random()* 255 +1);
+            var green=Math.floor(Math.random()* 255 +1);
 
-        random_number_r = Math.floor(Math.random()* 255 +1);
+            document.getElementById("sound_detecting_count").innerHTML= "Detected Barking - "+dog+" Detected Meowing - "+cat+" Detected Roaring - "+lion+" Detected Mooing - " + cow;
+            document.getElementById("result_sound").innerHTML= "Detected Sound Of - "+results;
 
-        random_number_g = Math.floor(Math.random()* 255 +1);
+            document.getElementById("result_sound").style.color = "rgb("+red+", "+green+", "+blue+")";
+            document.getElementById("sound_detecting_count").style.color = "rgb("+red+", "+green+", "+blue+")";
 
-        random_number_b = Math.floor(Math.random()* 255 +1);
+            img = document.getElementById("photo");
 
-        document.getElementById("result_label").innerHTML = "I can Hear - " + results[0].label;
-        document.getElementById("result_confidence").innerHTML = "Accuracy - " + (results[0].confidence * 100).toFixed(2)+" % ";
-        document.getElementById("result_label").style.color = "rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
-        document.getElementById("result_confidence").style.color = "rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
-        
-        img = document.getElementById("alien1");
-        img1 = document.getElementById("alien2");
-        img2 = document.getElementById("alien3");
-        img3 = document.getElementById("alien4");
+            if(results[0].label == "barking"){
 
-        if(results[0].label == "Clap"){
+                img.src="dog.jpeg";
+                dog = dog + 1;
 
-            img.src="aliens-01.gif";
-            img1.src="aliens-02.png";
-            img2.src="aliens-03.png";
-            img3.src="aliens-04.png";
+    
+            }
+    
+            else if(results[0].label == "meowing"){
+    
+                img.src="cat.png";
+                cat = cat+1;
 
-        }
+            }
+    
+            else if(results[0].label == "roaring"){
+    
+                img.src="lion.png";
+                lion = lion+1;
+    
+            }
 
-        else if(results[0].label == "Bell"){
+            else if(results[0].label == "mooing"){
+    
+                img.src="cow.png";
+                cow = cow+1;
+    
+            }
+    
+            else {
+    
+                img.src="ear.png";
+            }
+    
+    
 
-            img.src="aliens-01.png";
-            img1.src="aliens-02.gif";
-            img2.src="aliens-03.png";
-            img3.src="aliens-04.png";
+        } }
 
-        }
+var dog=0
+var cat=0
+var cow=0
+var lion=0
 
-        else if(results[0].label == "Snapping"){
-
-            img.src="aliens-01.png";
-            img1.src="aliens-02.png";
-            img2.src="aliens-03.gif";
-            img3.src="aliens-04.png";
-
-        }
-
-        else {
-
-            img.src="aliens-01.png";
-            img1.src="aliens-02.png";
-            img2.src="aliens-03.png";
-            img3.src="aliens-04.gif";
-
-        }
-
-
-
-     }
